9ed320c70d2c671a9646533178c743f7d9c3a35a
Jordan Krueger <jkrueger702@gmail.com>
2018-05-17 05:15:25 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,10 +2,8 @@ def using_until
         levitation_force = 6
         while levitation_force < 10
 					leviation_force += 1
-
-		 puts "Wingardium Leviosa"
+					puts "Wingardium Leviosa"
    end
 end
 
 puts "You levitated the feather!"
-
